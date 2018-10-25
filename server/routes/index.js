c9ff963f6f30c6a3eccc9a4ad8367d2eee207bb2
Chidinma Orajiaku <corajiaku96@gmail.com>
2018-10-25 01:18:51 +0100

    update a contact

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import ContactController from '../controller/contactController';
 
 const router = express.Router();
-const { createContact, getContacts, deleteContact } = ContactController;
+const { createContact, getContacts, deleteContact, updateContact } = ContactController;
 
 router.post('/users', createContact);
 router.get('/users', getContacts)
 router.delete('/users/:id', deleteContact)
+router.put('/users/:id', updateContact)
 
 export default router;
