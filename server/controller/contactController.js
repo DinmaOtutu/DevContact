5ca27d426838282ae3416ec688a87c87a04b1196
Chidinma Orajiaku <corajiaku96@gmail.com>
2018-10-26 01:27:21 +0100

    add comments

--- server/controller/contactController.js
+++ server/controller/contactController.js
@@ -8,9 +8,10 @@
  */
 class ContactController {
     /**
-     * class for creating the contact details
+     * class for creating new contact details
      */
    static createContact(req, res) {
+       // destructure input fields
        const {
            username,
            password, 
@@ -20,7 +21,8 @@ class ContactController {
        } = req.body;
      
       return Contacts.create({
-           username, fullname, email, category, password: hashSync(password, 10)
+           username, fullname, email, category,
+           password: hashSync(password, 10) // hash password
        }).then((newUser) => {
            const user = {
                id: newUser.id,
@@ -28,60 +30,83 @@ class ContactController {
                email: newUser.email,
                category: newUser.category
            }
+           // response on success
             return res.status(201).json({
                message: 'signed up successfully',
                user
                
            })
-       }).catch((error) => {
+       })
+       // catch error and return to user
+       .catch((error) => {
            return res.status(400).json({
                message: error.message
            })
        })
     }
 
+    /**
+     * class for getting a contact
+     */
    static getContacts(req, res) {
-     Contacts.all().then((contacts) => {
-         if(contacts.length === 0 ) {
-             return res.status(404).json({
-                 message: 'contacts not found'
-             })
-         }
-         const allContacts = contacts.map(contact => ({
-             id: contact.id,
-             fullname: contact.fullname,
-             email: contact.email,
-             category: contact.category,
+     Contacts
+        // find all contacts
+        .all().then((contacts) => {
+            // if no contact
+            if(contacts.length === 0 ) {
+                return res.status(404).json({
+                    message: 'contacts not found'
+                })
+            }
+            // loop through contacts
+            const allContacts = contacts.map(contact => ({
+                id: contact.id,
+                fullname: contact.fullname,
+                email: contact.email,
+                category: contact.category,
 
-        }))
-        return res.status(200).json({
-            message: 'All contacts',
-            allContacts
-        });
+            }))
+            return res.status(200).json({
+                message: 'All contacts',
+                allContacts
+            });
    });
    };
 
+   /**
+     * class for creating new contact details
+     */
   static deleteContact(req, res) {
+      // destrucutre req params
       const { id } = req.params;
-      Contacts.findOne({
-          where: {
-              id,
-          }
-      }).then((contact) => {
-          if(!contact) {
-              return res.status(404).json({
-                  message: 'contact not found'
-              })
-          }
-          contact.destroy().then(deletedContact => {
-              return res.status(200).json({
-                  message: 'contact successfully deleted'
-              })
-          })
-      })
+      Contacts
+        // find one user
+        .findOne({
+            where: {
+                id,
+            }
+        }).then((contact) => {
+            // no contact found
+            if(!contact) {
+                return res.status(404).json({
+                    message: 'contact not found'
+                })
+            }
+            contact
+                // delete contact
+                .destroy().then(deletedContact => {
+                    return res.status(200).json({
+                        message: 'contact successfully deleted'
+                    })
+                })
+        })
   }
 
+  /**
+     * class for creating new contact details
+     */
   static updateContact(req, res) {
+      // destructure inputs
     const {
         username,
         password,
@@ -90,60 +115,80 @@ class ContactController {
         category,
     } = req.body;
 
+    // destructure req params
     const { id } = req.params;
-    Contacts.findOne({
-        where: {
-            id,
-        }
-    }).then(contact => {
-        if(!contact) {
-            return res.status(404).json({
-                message: 'contact not found'
-            })
-        }
-        contact.update({
-            username, fullname, email, category, password: hashSync(password, 10)
-        }).then((updatedUser) => {
-            const user = {
-                id: updatedUser.id,
-                fullname: updatedUser.fullname,
-                email: updatedUser.email,
-                category: updatedUser.category
-            }
-            return res.status(200).json({
-                message: 'contact updated successfully',
-                user
-            })
-        }).catch((error) => {
-            return res.status(400).json({
-                message: error.message
-            })
-        });
-    })}
-
-    static singleContact(req, res) {
-        const { id } = req.params;
-        Contacts.findOne({
+    return Contacts
+        // find one contact
+        .findOne({
             where: {
                 id,
             }
-        }).then((contact) => {
+        })
+        // on success
+        .then(contact => {
+            // no contact found
             if(!contact) {
                 return res.status(404).json({
                     message: 'contact not found'
                 })
             }
-            const user = {
-                id: contact.id,
-                fullname: contact.fullname,
-                email: contact.email,
-                category: contact.category
-            }
-            return res.status(200).json({
-                message: 'contact found successfully',
-                user
+            contact
+                // update contact
+                .update({
+                    username, fullname, email, category, password: hashSync(password, 10)
+                }).then((updatedUser) => {
+                    const user = {
+                        id: updatedUser.id,
+                        fullname: updatedUser.fullname,
+                        email: updatedUser.email,
+                        category: updatedUser.category
+                    }
+                    // return contact on success
+                    return res.status(200).json({
+                        message: 'contact updated successfully',
+                        user
+                    })
+                })
+                // catch error
+                .catch((error) => {
+                    return res.status(400).json({
+                        message: error.message
+                    })
+                });
+    })}
+
+    /**
+     * class for getting one contact
+     */
+    static singleContact(req, res) {
+        const { id } = req.params;
+        Contacts
+            // find one user
+            .findOne({
+                where: {
+                    id,
+                }
+        })
+            // on success
+            .then((contact) => {
+                // no contact found
+                if(!contact) {
+                    return res.status(404).json({
+                        message: 'contact not found'
+                    })
+                }
+                const user = {
+                    id: contact.id,
+                    fullname: contact.fullname,
+                    email: contact.email,
+                    category: contact.category
+                }
+                // return user
+                return res.status(200).json({
+                    message: 'contact found successfully',
+                    user
+                });
             });
-        });
     }
 }
 
